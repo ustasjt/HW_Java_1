905c7994d5e2f6b37276557329d590694178c307
ustas <ustas-jt@yandex.ru>
2023-11-10 03:13:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 13_676;
+        int step = 20;
+        System.out.println((amount /step)+" миля(и)");
+
+    }
+
+}
